f25a195d20f08ece6e631d9dfc89b67a0cb8d26b
thajames1377 <anderson>
2024-07-01 01:59:22 -0300

    meu commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const forms = document.querySelectorAll('form');
+    
+    forms.forEach(form => {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            alert('Formulário enviado!');
+        });
+    });
+});
